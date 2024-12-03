e0211f5d1db75df3c83b36093867cdd23cf7b7dc
krishbedi17 <106929134+krishbedi17@users.noreply.github.com>
2024-12-02 19:22:14 -0800

    Error Calulations

---
+++ get_season_schedule.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import requests
+
+def get_season_schedule(year):
+    url = f"http://ergast.com/api/f1/{year}.json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        schedule = response.json().get('MRData', {}).get('RaceTable', {}).get('Races', [])
+        if schedule:
+            schedule_df = pd.json_normalize(schedule)
+            schedule_df = schedule_df[[
+                'round', 'raceName', 'Circuit.circuitName', 'Circuit.Location.locality',
+                'Circuit.Location.country', 'date', 'time'
+            ]]
+            schedule_df.rename(columns={
+                'round': 'Round',
+                'raceName': 'Race Name',
+                'Circuit.circuitName': 'Circuit Name',
+                'Circuit.Location.locality': 'Locality',
+                'Circuit.Location.country': 'Country',
+                'date': 'Date',
+                'time': 'Time'
+            }, inplace=True)
+            return schedule_df
+        else:
+            print(f"No data available for the year {year}.")
+            return pd.DataFrame()
+
+    else:
+        print(f"Failed to fetch data: {response.status_code}")
+        return []
+
+
+def main():
+    for i in range(2018, 2024):
+        schedule = get_season_schedule(i)
+        schedule.to_csv(f"season_schedule/{i}_season_schedule.csv", index=False)
+
+main()
